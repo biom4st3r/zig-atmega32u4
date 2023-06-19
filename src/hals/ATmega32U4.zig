020ff24bb6c712e88ca23129b972b95eada884a2
Biom4st3r <biom4st3r@gmail.com>
2023-06-18 20:30:05 -0500

    * test program

---
+++ src/hals/ATmega32U4.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const micro = @import("microzig");
+
+pub fn parse_pin(comptime spec: []const u8) type {
+    if ()
+}
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,21 @@
 const micro = @import("microzig");
+const unstable = micro.core.experimental;
+//https://zeg.random-projects.net/getting-started.htm
+//https://github.com/ZigEmbeddedGroup/microzig/blob/main/test/programs/blinky.zig
+pub fn main() void {
+    const led_pin = unstable.Pin("D13");
+    const led = unstable.gpio.Gpio(led_pin, .{ .mode = .output, .initial_state = .low });
+    led.init();
+    while (true) {
+        busyloop();
+        led.toggle();
+    }
+}
+fn busyloop() void {
+    const limit = 100_000;
 
-pub fn main() void {}
+    var i: u24 = 0;
+    while (i < limit) : (i += 1) {
+        @import("std").mem.doNotOptimizeAway(i);
+    }
+}
